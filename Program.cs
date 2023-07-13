d5950ba2f09a33b0ca5edb237d017ff743446d00
2Sravs <sravani.chekuri1243#gmail.com>
2023-07-13 10:14:56 +0530

    practice

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = new int [5];
+            Console.WriteLine("enter marks");
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine("enter marks in subject " + (i + 1));
+                marks[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("*marks list*");
+            for(int i = 0;i<marks.Length;i++)
+            {
+                Console.WriteLine($"Subject {i + 1} \t Marks: {marks[i]}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
